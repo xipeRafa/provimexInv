ef69bb2d4fbfa779501831f7398dd05f0150be5e
fer <fer@fers-iMac.local>
2025-03-19 16:30:01 -0700

    ss

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -11,13 +11,20 @@ import { getStorage } from 'firebase/storage';
 //require("dotenv").config()
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDxFuYvYPsm53Q_5uvq5GZmy9qefOi7t_U",
-  authDomain: "trip-advisor-2759d.firebaseapp.com",
-  projectId: "trip-advisor-2759d",
-  storageBucket: "trip-advisor-2759d.appspot.com",
-  messagingSenderId: "756306939148",
-  appId: "1:756306939148:web:362df3149468ebde6f8d29",
-  measurementId: "G-5SLMPW8877"
+  // apiKey: "AIzaSyDxFuYvYPsm53Q_5uvq5GZmy9qefOi7t_U",
+  // authDomain: "trip-advisor-2759d.firebaseapp.com",
+  // projectId: "trip-advisor-2759d",
+  // storageBucket: "trip-advisor-2759d.appspot.com",
+  // messagingSenderId: "756306939148",
+  // appId: "1:756306939148:web:362df3149468ebde6f8d29",
+  // measurementId: "G-5SLMPW8877"
+
+  apiKey: "AIzaSyB3s6QjYWR6EL45XSK7ANP-D_1I-DZ9eYg",
+  authDomain: "taxis-e44d6.firebaseapp.com",
+  projectId: "taxis-e44d6",
+  storageBucket: "taxis-e44d6.appspot.com",
+  messagingSenderId: "603667662446",
+  appId: "1:603667662446:web:0014b763a2a4acca92487f"
 });
 
 export const authApp = getAuth(app);
